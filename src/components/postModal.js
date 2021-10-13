33961198b00098d140b10aee448f2329366896c1
Mansuur Abdullahi Abdirahman <mansuurtech101@gmail.com>
2021-10-13 21:41:25 +0300

    lol

--- src/components/postModal.js
+++ src/components/postModal.js
@@ -2,10 +2,35 @@ import styled from "styled-components";
 
 const PostModal = () => {
   return (
-    <div>
-      <h1>Post</h1>
-    </div>
+    <Container>
+      <Content></Content>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 999;
+  color: black;
+  background: rgba(0, 0, 0, 0.8);
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 552px;
+  background-color: #fff;
+  max-height: 99%;
+  overflow: hidden;
+  border-radius: 5px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  top: 32px;
+  margin: 0 auto;
+`;
 
 export default PostModal;
